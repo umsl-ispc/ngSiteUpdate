f2e13b9fceb938e866617faab18e0943bc01cc3c
Lein-Haz <paz5x4@mail.umsl.edu>
2015-10-07 15:52:49 -0500

    package.json updated

--- build.config.js
+++ build.config.js
@@ -77,13 +77,11 @@ module.exports = {
       'vendor/angular-material/angular-material.min.js',
       'vendor/angular-animate/angular-animate.min.js',
       'vendor/angular-aria/angular-aria.min.js',
-      'vendor/angular-ui-grid/ui-grid.min.js',
 
     ],
     css: [
       'vendor/angular-ui-grid/ui-grid.min.css',
       'vendor/angular-material/angular-material.min.css',
-      'vendor/angular-ui-grid/ui-grid.min.css',
     ],
     assets: [
       'vendor/angular-ui-grid/ui-grid.svg',
